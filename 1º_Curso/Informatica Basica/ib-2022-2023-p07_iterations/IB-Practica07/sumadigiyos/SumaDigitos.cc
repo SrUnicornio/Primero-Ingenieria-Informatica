25bfd5c99f91b96d0abe2938f1ecda761fe485c1
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:22:55 +0000

    Update SumaDigitos.cc

--- 1º_Curso/Informatica Basica/ib-2022-2023-p07_iterations/IB-Practica07/sumadigiyos/SumaDigitos.cc
+++ 1º_Curso/Informatica Basica/ib-2022-2023-p07_iterations/IB-Practica07/sumadigiyos/SumaDigitos.cc
@@ -1,16 +1,3 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica 2022-2023
- *
- * @file SumaDigitos.cc
- * @author Alvao Perez Ramos alu0101574042@ull.edu.es
- * @date Nov 9 2022
- * @brief Write a function that tells if a natural n is perfect.
- * @bug There are no known bugs
- * @see https://github.com/IB-2022-2023/P07-iterations/blob/main/iterations.md
- */
 #include <iostream>
 using namespace std;
 
