b6ca478b6166e453cc4ac0976dd6d6de09f2984e
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:26:05 +0000

    Update P97156_NumbersInterval.cc

--- 1º_Curso/Informatica Basica/ib-2022-2023-p06_alternatives_and_iterations/IB-Practica06/Interval/P97156_NumbersInterval.cc
+++ 1º_Curso/Informatica Basica/ib-2022-2023-p06_alternatives_and_iterations/IB-Practica06/Interval/P97156_NumbersInterval.cc
@@ -1,22 +1,7 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica 2022-2023
- *
- * @file  Numbers_inan_Interval.cc
- * @author Alvao Perez Ramos alu0101574042@ull.edu.es
- * @date Oct 22 2022
- * @brief Write a program that reads two numbers a and b, and prints all numbers
- * between a and b.
- * @bug There are no known bugs
- * @see https://jutge.org/problems/P97156
- */
 #include <iostream>
 using namespace std;
 int main() {
   int Num1, Num2, Num3;
-
   cin >> Num1 >> Num2;
 
   if (Num1 == Num2) {
@@ -27,11 +12,9 @@ int main() {
     cout << endl;
     return 0;
   }
-
   do {
     printf("%d%s", Num1, ",");
     Num1 = Num1 + 1;
-
   } while (Num1 != Num2);
   printf("%d\n", Num1);
   return 0;
