f9a3e3b1e02d6ded05c7a5bd3f9bd20e60c16e99
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:23:56 +0000

    Update Problema3.cc

--- 1º_Curso/Informatica Basica/ib-2022-2023-p07_iterations/p07-interations/Problema3/Problema3.cc
+++ 1º_Curso/Informatica Basica/ib-2022-2023-p07_iterations/p07-interations/Problema3/Problema3.cc
@@ -1,17 +1,3 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica 2022-2023
- *
- * @file Problema3.cc
- * @author Alvao Perez Ramos alu0101574042@ull.edu.es
- * @date Nov 18 2022
- * @brief Write a program to compute powers.
- * @bug There are no known bugs
- * @see https://jutge.org/problems/P798177
- */
-
 #include <iostream>
 using namespace std;
 
