c66b7666df79611d5496a54f3fdea400293458ba
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:25:24 +0000

    Update P90615_Maximum_3_DifferentIntegerNumbers.cc

--- 1º_Curso/Informatica Basica/ib-2022-2023-p06_alternatives_and_iterations/IB-Practica06/DiferentNumber/P90615_Maximum_3_DifferentIntegerNumbers.cc
+++ 1º_Curso/Informatica Basica/ib-2022-2023-p06_alternatives_and_iterations/IB-Practica06/DiferentNumber/P90615_Maximum_3_DifferentIntegerNumbers.cc
@@ -1,18 +1,3 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica 2022-2023
- *
- * @file Maximo of three integer numbers.cc
- * @author Alvao Perez Ramos alu0101574042@ull.edu.es
- * @date Oct 22 2022
- * @brief Write a program that reads three numbers, all different, and prints
- * their maximum. Write a program that reads three numbers, all different, and
- * prints their maximum.
- * @bug There are no known bugs
- * @see https://jutge.org/problems/P90615
- */
 #include <algorithm>
 #include <iostream>
 using namespace std;
