98f4abff8964818d1553c4ec7b708322c6c529b6
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:12:17 +0000

    Update hello_world.cc

--- 1º_Curso/Informatica Basica/P04-first-programs/jutge/hello_world.cc
+++ 1º_Curso/Informatica Basica/P04-first-programs/jutge/hello_world.cc
@@ -1,17 +1,3 @@
-/*
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica
- *
- * @author F. de Sande
- * @date 4 Jun 2020
- * @brief The classical 'Hello World' first program in any language
- *
- * @see http://www.cplusplus.com/doc/tutorial/program_structure/
- * @see http://cpp.sh/2dd
- */
-
 #include <iostream>
 
 int main () {
