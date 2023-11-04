6d6ba056d87abd5ce25d22615a651b63a379452f
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:40:27 +0000

    Update sll_t.h

--- 1º_Curso/Algoritmos y estructura de datos/Ayed Practicas/Practica 4/sll_t.h
+++ 1º_Curso/Algoritmos y estructura de datos/Ayed Practicas/Practica 4/sll_t.h
@@ -1,6 +1,6 @@
-// AUTOR: Álvaro Pérez Ramos
+// AUTOR:
 // FECHA: 30/03/2023
-// EMAIL: alu0101574042@ull.edu.es
+// EMAIL:
 // VERSION: 3.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 4
