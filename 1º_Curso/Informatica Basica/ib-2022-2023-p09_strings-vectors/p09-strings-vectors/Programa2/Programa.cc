69e88d91cb9bfdc2da8e39904712e47ff881381d
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:20:38 +0000

    Update Programa.cc

--- 1º_Curso/Informatica Basica/ib-2022-2023-p09_strings-vectors/p09-strings-vectors/Programa2/Programa.cc
+++ 1º_Curso/Informatica Basica/ib-2022-2023-p09_strings-vectors/p09-strings-vectors/Programa2/Programa.cc
@@ -1,15 +1,3 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básaica 2022-2023
- *
- * @file  Programa.cc
- * @author Alvaro Perez Ramos alu0101574042@ull.edu.es
- * @date Dic 2 2022
- * @brief 
- */
-
 #include <iostream>
 using namespace std;
 
