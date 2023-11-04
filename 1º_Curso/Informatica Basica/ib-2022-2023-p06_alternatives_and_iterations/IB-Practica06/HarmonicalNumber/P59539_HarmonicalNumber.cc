b68f2ed6dedba78c488cbba4f6185046a6268a27
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:25:40 +0000

    Update P59539_HarmonicalNumber.cc

--- 1º_Curso/Informatica Basica/ib-2022-2023-p06_alternatives_and_iterations/IB-Practica06/HarmonicalNumber/P59539_HarmonicalNumber.cc
+++ 1º_Curso/Informatica Basica/ib-2022-2023-p06_alternatives_and_iterations/IB-Practica06/HarmonicalNumber/P59539_HarmonicalNumber.cc
@@ -1,18 +1,3 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica 2022-2023
- *
- * @file HarmonicalNumber.cc
- * @author Alvao Perez Ramos alu0101574042@ull.edu.es
- * @date Oct 22 2022
- * @brief Write a program that reads a number n and prints the n-th harmonic
- *number, defined as Hn = 1/1 + 1/2 + ⋯ + 1/n. Print Hn with 4 digits after the
- *decimal point
- * @bug There are no known bugs
- * @see https://jutge.org/problems/P59539
- */
 #include <iomanip> //al incluir iomanip podemos manipular diferntes parametros que requiera el porgrma
 #include <iostream> 
 
