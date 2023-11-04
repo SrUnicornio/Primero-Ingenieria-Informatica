e3d2f5bc9015944bbec4c7f3ae406ada611bd375
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:39:17 +0000

    Update sparse_vector_t.h

--- 1º_Curso/Algoritmos y estructura de datos/Ayed Practicas/Practica 3/sparse_vector_t.h
+++ 1º_Curso/Algoritmos y estructura de datos/Ayed Practicas/Practica 3/sparse_vector_t.h
@@ -1,6 +1,6 @@
-// AUTOR: Álvaro Pérez Ramos
+// AUTOR:
 // FECHA: 13/03/2023
-// EMAIL: alu0101574042@ull.edu.es
+// EMAIL:
 // VERSION: 4.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 3
