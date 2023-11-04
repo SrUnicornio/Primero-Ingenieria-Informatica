fa1ce5b04b59b6dd1e05638c3675f06c1453b75e
Álvaro <118526115+SrUnicornio@users.noreply.github.com>
2023-11-04 20:37:03 +0000

    Update rational_t.cpp

--- 1º_Curso/Algoritmos y estructura de datos/Ayed Practicas/Practica 1/rational_t.cpp
+++ 1º_Curso/Algoritmos y estructura de datos/Ayed Practicas/Practica 1/rational_t.cpp
@@ -1,6 +1,6 @@
-// AUTOR: Álvaro Pérez Ramos
+// AUTOR: 
 // FECHA: 14/02/2023
-// EMAIL: alu0101574042@ull.edu.es
+// EMAIL: 
 // VERSION: 1.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 1
